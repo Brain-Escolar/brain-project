e46be6d42722863b7fb23c23646ca65a534f7c6a
Mateus Bittencourt <bittenmat@gmail.com>
2026-03-15 23:10:20 -0300

    Forçando action a rodar

--- backend/src/main/java/br/com/brain/service/CoordenadorService.java
+++ backend/src/main/java/br/com/brain/service/CoordenadorService.java
@@ -19,86 +19,90 @@
 // @RequiredArgsConstructor
 // public class CoordenadorService {
 
-//     private final CoordenadorRepository repository;
-//     private final EnderecoService enderecoService;
-//     private final UsuarioService usuarioService;
+// private final CoordenadorRepository repository;
+// private final EnderecoService enderecoService;
+// private final UsuarioService usuarioService;
 
-//     @PersistenceContext
-//     private EntityManager em;
+// @PersistenceContext
+// private EntityManager em;
 
-//     @Transactional
-//     public Coordenador cadastrarCoordenador(CadastroCoordenadorDto dados) {
+// @Transactional
+// public Coordenador cadastrarCoordenador(CadastroCoordenadorDto dados) {
 
-//         var coordenador = new Coordenador();
-//         coordenador.setCpf(dados.cpf());
-//         coordenador.setNome(dados.nome());
-//         coordenador.setEmail(dados.email());
-//         coordenador.setEmailProfissional(dados.cpf() + "@email.com");
-//         coordenador.setDataDeNascimento(dados.dataDeNascimento());
-//         coordenador.setEndereco(enderecoService.preencherEnderco(dados.endereco()));
-//         coordenador.setRg(dados.rg());
-//         coordenador.setCarteiraDeTrabalho(dados.carteiraDeTrabalho());
+// var coordenador = new Coordenador();
+// coordenador.setCpf(dados.cpf());
+// coordenador.setNome(dados.nome());
+// coordenador.setEmail(dados.email());
+// coordenador.setEmailProfissional(dados.cpf() + "@email.com");
+// coordenador.setDataDeNascimento(dados.dataDeNascimento());
+// coordenador.setEndereco(enderecoService.preencherEnderco(dados.endereco()));
+// coordenador.setRg(dados.rg());
+// coordenador.setCarteiraDeTrabalho(dados.carteiraDeTrabalho());
 
-//         usuarioService.cadastrarUsuario(
-//                 coordenador.getNome(),
-//                 PerfilNome.COORDENADOR,
-//                 coordenador.getEmail(),
-//                 coordenador.getEmailProfissional(),
-//                 coordenador.getCpf());
+// usuarioService.cadastrarUsuario(
+// coordenador.getNome(),
+// PerfilNome.COORDENADOR,
+// coordenador.getEmail(),
+// coordenador.getEmailProfissional(),
+// coordenador.getCpf());
 
-//         repository.save(coordenador);
+// repository.save(coordenador);
 
-//         return coordenador;
-//     }
+// return coordenador;
+// }
 
-//     public Page<ListagemCoordenadorDto> listar(Pageable paginacao) {
-//         return repository.findAll(paginacao).map(ListagemCoordenadorDto::new);
-//     }
+// public Page<ListagemCoordenadorDto> listar(Pageable paginacao) {
+// return repository.findAll(paginacao).map(ListagemCoordenadorDto::new);
+// }
 
-//     @Transactional
-//     public Coordenador atualizar(AtualizacaoCoordenadorDto dados, Long id) {
-//         var coordenador = repository
-//                 .findById(id)
-//                 .orElseThrow(
-//                         () -> new EntityNotFoundException("Coordenador de id " + id + " não existe."));
+// @Transactional
+// public Coordenador atualizar(AtualizacaoCoordenadorDto dados, Long id) {
+// var coordenador = repository
+// .findById(id)
+// .orElseThrow(
+// () -> new EntityNotFoundException("Coordenador de id " + id + " não
+// existe."));
 
-//         if (dados.nome() != null) {
-//             coordenador.setNome(dados.nome());
-//         }
-//         if (dados.dataDeNascimento() != null) {
-//             coordenador.setDataDeNascimento(dados.dataDeNascimento());
-//         }
-//         if (dados.email() != null) {
-//             coordenador.setEmail(dados.email());
-//         }
-//         if (dados.endereco() != null) {
-//             var endereco = enderecoService.atualizarEndereco(coordenador.getEndereco(), dados.endereco());
-//             coordenador.setEndereco(endereco);
-//         }
-//         if (dados.rg() != null) {
-//             coordenador.setRg(dados.rg());
-//         }
-//         if (dados.carteiraDeTrabalho() != null) {
-//             coordenador.setCarteiraDeTrabalho(dados.carteiraDeTrabalho());
-//         }
+// if (dados.nome() != null) {
+// coordenador.setNome(dados.nome());
+// }
+// if (dados.dataDeNascimento() != null) {
+// coordenador.setDataDeNascimento(dados.dataDeNascimento());
+// }
+// if (dados.email() != null) {
+// coordenador.setEmail(dados.email());
+// }
+// if (dados.endereco() != null) {
+// var endereco = enderecoService.atualizarEndereco(coordenador.getEndereco(),
+// dados.endereco());
+// coordenador.setEndereco(endereco);
+// }
+// if (dados.rg() != null) {
+// coordenador.setRg(dados.rg());
+// }
+// if (dados.carteiraDeTrabalho() != null) {
+// coordenador.setCarteiraDeTrabalho(dados.carteiraDeTrabalho());
+// }
 
-//         repository.save(coordenador);
+// repository.save(coordenador);
 
-//         return coordenador;
-//     }
+// return coordenador;
+// }
 
-//     @Transactional
-//     public void excluir(Long id) {
-//         var coordenador = repository
-//                 .findById(id)
-//                 .orElseThrow(
-//                         () -> new EntityNotFoundException("Coordenador de id " + id + " não existe."));
-//         repository.delete(coordenador);
-//     }
+// @Transactional
+// public void excluir(Long id) {
+// var coordenador = repository
+// .findById(id)
+// .orElseThrow(
+// () -> new EntityNotFoundException("Coordenador de id " + id + " não
+// existe."));
+// repository.delete(coordenador);
+// }
 
-//     public Coordenador detalhar(Long id) {
-//         return repository
-//                 .findById(id)
-//                 .orElseThrow(() -> new EntityNotFoundException("Coordenador de id " + id + " não existe."));
-//     }
+// public Coordenador detalhar(Long id) {
+// return repository
+// .findById(id)
+// .orElseThrow(() -> new EntityNotFoundException("Coordenador de id " + id + "
+// não existe."));
+// }
 // }
